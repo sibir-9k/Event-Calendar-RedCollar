df83095bb5307a0c055b5e7ce6dec723b6cefa5b
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-10-18 00:07:52 +0300

    fix: link for svg icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	resolve: {
 		alias: {
-			'@/public/': '/public/',
+			'@/public/images': path.resolve(__dirname, 'public/images/'),
 		},
 	},
 });
